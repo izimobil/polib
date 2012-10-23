be5dedf5845906129e7ae6b0c9cdc0d20173c734
David Jean Louis <izimobil@gmail.com>
2012-10-23 12:41:07 +0200

    Release 1.0.2

--- polib.py
+++ polib.py
@@ -13,7 +13,7 @@ modify entries, comments or metadata, etc. or create new po files from scratch.
 """
 
 __author__    = 'David Jean Louis <izimobil@gmail.com>'
-__version__   = '1.0.1'
+__version__   = '1.0.2'
 __all__       = ['pofile', 'POFile', 'POEntry', 'mofile', 'MOFile', 'MOEntry',
                  'detect_encoding', 'escape', 'unescape', 'detect_encoding',]
 
