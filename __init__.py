6214ba2315d80506bff545513682d9304d83134d
David Jean Louis <izimobil@gmail.com>
2012-12-06 11:28:53 +0100

    Added __init__.py file for convenience (see #39)

---
+++ __init__.py
@@ -0,0 +1,5 @@
+# This __init__.py is only here for convenience, for projects that include a
+# version of polib as a mercurial sub-repository for example.
+#
+# This file will NOT be installed when installing polib with pip, setuptools,
+# or any other mecanism based on the setup.py file.
