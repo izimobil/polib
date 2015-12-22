72277ff2319ec2faa29bc8a8a6c488533e693334
David Jean Louis <izimobil@gmail.com>
2015-12-22 10:54:35 +0100

    Fixed issue #71 (tcomment and flags not saved for obsolete entries)

---
+++ Makefile
@@ -0,0 +1,24 @@
+# polib Makefile, useful for developers only.
+# Make sure you have pep8 and tox python modules installed.
+
+.PHONY: clean lint test dist
+
+all: lint test clean
+
+clean:
+	@find . -name '*.pyc' |xargs rm -f
+	@find . -name '*.pyo' |xargs rm -f
+	@find . -name '*.orig' |xargs rm -f
+	@rm -rf MANIFEST build dist .coverage .tox __pycache__ docs/_build *.egg-info
+
+lint:
+	@type pep8 >/dev/null 2>&1 || { echo >&2 "Please install pep8 package."; exit 1; }
+	@pep8 -r polib.py && { echo >&2 "PEP8: congrats, everything is clean !"; }
+
+test:
+	@type tox >/dev/null 2>&1 && { tox; } || { ./runtests.sh; }
+
+dist: clean
+	@python setup.py register
+	@python setup.py sdist upload
+	@python setup.py bdist_wheel upload
---
+++ setup.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# License: MIT (see LICENSE file provided)
+# vim600: fdm=marker tabstop=4 shiftwidth=4 expandtab ai
+
+"""
+polib setup script.
+"""
+
+__author__ = 'David Jean Louis <izimobil@gmail.com>'
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import codecs
+import polib
+
+author_data = __author__.split(' ')
+maintainer = ' '.join(author_data[0:-1])
+maintainer_email = author_data[-1][1:-1]
+desc = 'A library to manipulate gettext files (po and mo files).'
+
+if polib.PY3:
+    enc = {'encoding': 'UTF-8'}
+else:
+    enc = {}
+
+long_desc = r'''
+%s
+
+%s
+
+''' % (open('README.rst', **enc).read(), open('CHANGELOG', **enc).read())
+
+if __name__ == '__main__':
+    setup(
+        name='polib',
+        description=desc,
+        long_description=long_desc,
+        version=polib.__version__,
+        author=maintainer,
+        author_email=maintainer_email,
+        maintainer=maintainer,
+        maintainer_email=maintainer_email,
+        url='http://bitbucket.org/izi/polib/',
+        download_url='https://pypi.python.org/packages/source/p/polib/polib-%s.tar.gz' % polib.__version__,
+        license='MIT',
+        platforms=['posix'],
+        classifiers = [
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.5',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development :: Internationalization',
+            'Topic :: Software Development :: Localization',
+            'Topic :: Text Processing :: Linguistic'
+        ],
+        py_modules=['polib']
+    )
+
